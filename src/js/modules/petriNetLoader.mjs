9ede24b3e206e1c5bf5a393e2d224567d37472f7
Kasi <71724556+KasiKral@users.noreply.github.com>
2022-01-19 19:49:44 +0100

    using larvel mix to bundle all javascript

--- src/js/modules/petriNetLoader.mjs
+++ src/js/modules/petriNetLoader.mjs
@@ -1,76 +1,4 @@
-AFRAME.registerComponent("petri-net-sim", {
-    schema: {
-        currentState: { type: "string", default: "ST1" },
-        places: { type: "array", default: [] },
-        transitions: { type: "array", default: [] },
-        arcs: { type: "array", default: [] },
-        event: { type: "string", default: "" },
-        message: { type: "string", default: "" },
-    },
-    // Do something when component first attached.
-    init: function() {
-        var data = this.data;
-        this.eventHandlerFn = function() {
-            findNextState(data);
-        };
-        loadXMLDoc(data);
-        console.log(data);
-    },
-
-    update: function(oldData) {
-        // Do something when component's data is updated.
-        var data = this.data;
-        var el = this.el;
-
-        // `event` updated. Remove the previous event listener if it exists.
-        if (oldData.event && data.event !== oldData.event) {
-            el.removeEventListener(oldData.event, this.eventHandlerFn);
-        }
-
-        if (data.event) {
-            el.addEventListener(data.event, this.eventHandlerFn);
-        } else {
-            console.log(data.message);
-        }
-    },
-
-    remove: function() {
-        // Do something the component or its entity is detached.
-    },
-
-    // eslint-disable-next-line no-unused-vars
-    tick: function(time, timeDelta) {
-        // Do something on every scene tick or frame.
-    },
-});
-
-function findNextState(data) {
-    console.log("------FindingNextState-----");
-    console.log(data);
-    var choosedTransition = data.transitions.find(
-        (el) => el.name === data.message
-    );
-    console.log(choosedTransition);
-    var sourceTargetObj = data.arcs.find(
-        (el) => el.source === choosedTransition.id
-    );
-    console.log(sourceTargetObj);
-    var nextState = data.places.find((el) => el.id === sourceTargetObj.target);
-    console.log(nextState);
-    if (data.currentState !== nextState.name) {
-        data.currentState = nextState.name;
-        console.log(data);
-        openNextDoor(data.currentState);
-    }
-}
-
-function openNextDoor(nextState) {
-    var nextDoorEl = document.getElementById(nextState);
-    nextDoorEl.setAttribute("material", "color: green; shader: flat");
-    nextDoorEl.setAttribute("light", "type: point; color: green; intensity: 0.1");
-}
-
-function loadXMLDoc(data) {
+export function loadXMLDoc(data) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -99,6 +27,7 @@ function loadXMLDoc(data) {
     };
     xmlhttp.open("GET", "./petriNetFile/211021_cpn.pnml", true);
     xmlhttp.send();
+    console.log(data);
 }
 
 // Changes XML to JSON
---
+++ webpack.mix.js
@@ -0,0 +1,13 @@
+let mix = require("laravel-mix");
+
+mix
+    .js(
+        [
+            "src/js/components/petriNetSim.component.js",
+            "src/js/components/map.component.js",
+            "src/js/components/clickHandler.component.js",
+            "src/js/modules/petriNetLoader.mjs",
+        ],
+        "dist/bundle.js"
+    )
+    .setPublicPath("dist");
