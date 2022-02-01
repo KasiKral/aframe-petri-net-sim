0135ec2ebb70eae695c099a0223c2e38dee121b0
Kasi <71724556+KasiKral@users.noreply.github.com>
2022-02-01 22:35:06 +0100

    added scripts for building and running server

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.use("/dist", express.static(__dirname + "/dist"));
-app.use(express.static(__dirname + "/src"));
+app.use(express.static(__dirname + "/dist"));
+// app.use(express.static(__dirname + "/src"));
 
 app.listen(PORT, () =>
     console.log(`Server listening on port: http://localhost:${PORT}`)
