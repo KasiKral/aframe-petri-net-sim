5a6af9b4da8e9f5958d8df54e4afe35325e68326
Kasi <71724556+KasiKral@users.noreply.github.com>
2022-02-01 22:34:41 +0100

    building public folder

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,18 @@
 let mix = require("laravel-mix");
 
-mix
-    .js(
-        [
-            "src/js/components/petriNetSim.component.js",
-            "src/js/components/map.component.js",
-            "src/js/components/clickHandler.component.js",
-            "src/js/components/collisionDetector.component.js",
-            "src/js/modules/petriNetLoader.mjs",
-        ],
-        "dist/bundle.js"
-    )
-    .setPublicPath("dist");
+mix.copyDirectory("src/css/", "dist/css");
+mix.copyDirectory("src/img/", "dist/img");
+mix.copyDirectory("src/petriNetFile/", "dist/petriNetFile");
+mix.copy("src/js/splash.js", "dist/js");
+mix.copy("src/index.html", "dist/");
+
+mix.js(
+    [
+        "src/js/components/petriNetSim.component.js",
+        "src/js/components/map.component.js",
+        "src/js/components/clickHandler.component.js",
+        "src/js/components/collisionDetector.component.js",
+        "src/js/modules/petriNetLoader.mjs",
+    ],
+    "dist/js/bundle.js"
+);
