69c5e830fa71a5d0075a014a578f3bacbc97bb11
Kasi <kasela.adam@gmail.com>
2022-03-08 00:03:57 +0100

    migrated to bootstrap

--- src/js/splash.js
+++ src/js/splash.js
@@ -1,9 +1,5 @@
-// Workaround for mobile video
-// uses the splash screen button to trigger video play/pause so that the
-// video will auto play when the user enters the theater room
-const enterButton = document.querySelector('.splash__button')
-const splash = document.querySelector('.splash');
+import * as bootstrap from "bootstrap";
 
-enterButton.addEventListener('click', () => {
-  splash.style.display = 'none';
-});
+var myModalEl = document.querySelector("#myModal");
+var modal = bootstrap.Modal.getOrCreateInstance(myModalEl);
+modal.toggle();
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,27 +2,34 @@ let mix = require("laravel-mix");
 
 mix.disableNotifications();
 
-mix.copyDirectory("src/css/", "dist/css");
 mix.copyDirectory("src/img/", "dist/img");
 mix.copyDirectory("src/petriNetFile/", "dist/petriNetFile");
 mix.copyDirectory("src/views/", "dist/views");
-mix.copy("src/js/splash.js", "dist/js");
 
-mix.js(
-    [
-        "src/js/components/petriNetSim.component.js",
-        "src/js/components/map.component.js",
-        "src/js/components/clickHandler.component.js",
-        "src/js/components/collisionDetector.component.js",
-        "src/js/modules/petriNetLoader.mjs",
-        "src/js/modules/petriNet.mjs",
-        "src/js/models/sceneEvent.enum.js",
-    ],
-    "dist/js/bundle.js"
-);
+mix
+    .js(
+        [
+            "src/js/components/petriNetSim.component.js",
+            "src/js/components/map.component.js",
+            "src/js/components/clickHandler.component.js",
+            "src/js/components/collisionDetector.component.js",
+            "src/js/modules/petriNetLoader.mjs",
+            "src/js/modules/petriNet.mjs",
+            "src/js/models/sceneEvent.enum.js",
+        ],
+        "dist/js/bundle.js"
+    )
+    .sass("src/css/styles.scss", "dist/css");
+
+mix.js("src/js/splash.js", "dist/js/splash.js");
 
 mix.browserSync({
     injectChanges: false,
     proxy: "http://localhost:8080",
     files: ["dist/**/*"],
 });
+
+if (mix.inProduction()) {
+    mix.version();
+    mix.sourceMaps();
+}
