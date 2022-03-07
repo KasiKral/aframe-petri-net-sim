0588f0202fbd89604416df66275e66e6ec36a487
Kasi <kasela.adam@gmail.com>
2022-03-07 19:36:02 +0100

    server routes for pages

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = 8080;
+var dist = path.join(__dirname, "dist");
 
-app.use(express.static(__dirname + "/dist"));
-// app.use(express.static(__dirname + "/src"));
+app.use(express.static(dist));
+
+app.get("/", function(req, res) {
+    res.redirect("/home");
+});
+
+app.get("/home", function(req, res) {
+    res.sendFile(path.join(dist, "views/index.html"));
+});
+
+app.get("/scene", function(req, res) {
+    res.sendFile(path.join(dist, "views/scene.html"));
+});
 
 app.listen(PORT, () =>
     console.log(`Server listening on port: http://localhost:${PORT}`)
